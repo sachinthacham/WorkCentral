de6ea70f553c061a275d0fc910e4e812503b07c2
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-08 22:39:07 +0530

    import project schema, controller and service

--- api/src/modules/projects/projects.module.ts
+++ api/src/modules/projects/projects.module.ts
@@ -1,4 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Project, ProjectSchema } from './schemas/project.schema'
+import { ProjectsService } from './projects.service'
+import { ProjectsController } from './projects.controller'
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Project.name, schema: ProjectSchema }
+    ])
+  ],
+
+  controllers: [ProjectsController],
+  providers: [ProjectsService],
+
+})
 export class ProjectsModule {}
