c1953c4271aa9733d74df0d9a551afe36907c0d0
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-09 23:31:29 +0530

    implement task schema

---
+++ api/src/modules/tasks/schemas/task.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document, Types } from 'mongoose'
+
+export type TaskDocument = Task & Document
+
+@Schema({ timestamps: true })
+export class Task {
+
+  @Prop({ required: true })
+  title: string
+
+  @Prop()
+  description: string
+
+  @Prop({
+    enum: ['todo','in_progress','done'],
+    default: 'todo'
+  })
+  status: string
+
+  @Prop({
+    enum: ['low','medium','high'],
+    default: 'medium'
+  })
+  priority: string
+
+  @Prop({ type: Types.ObjectId, ref: 'Project' })
+  projectId: Types.ObjectId
+
+  @Prop({ type: Types.ObjectId, ref: 'Workspace' })
+  workspaceId: Types.ObjectId
+
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  assignee: Types.ObjectId
+
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  createdBy: Types.ObjectId
+
+  @Prop()
+  dueDate: Date
+
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task)
