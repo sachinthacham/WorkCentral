c9339d5c32316c9ab4aac530786df903c745e47f
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-09 23:31:15 +0530

    implement task dto

---
+++ api/src/modules/tasks/dto/create-task.dto.ts
@@ -0,0 +1,14 @@
+import { IsString } from 'class-validator'
+
+export class CreateTaskDto {
+
+  @IsString()
+  title: string
+
+  @IsString()
+  description: string
+
+  @IsString()
+  projectId: string
+
+}
