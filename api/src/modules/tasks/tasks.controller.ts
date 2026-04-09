46c78b166fa6b8cd49aad04525e447df095f7787
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-09 23:30:31 +0530

    implement create, get tasks methods

---
+++ api/src/modules/tasks/tasks.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Request,
+  UseGuards
+} from '@nestjs/common'
+
+import { TasksService } from './tasks.services'
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard'
+import { CreateTaskDto } from './dto/create-task.dto'
+
+@Controller('tasks')
+export class TasksController {
+
+  constructor(private tasksService:TasksService){}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  createTask(@Body() dto:CreateTaskDto,@Request() req){
+
+    const workspaceId = req.headers["workspaceid"]
+
+    return this.tasksService.createTask(
+      dto.title,
+      dto.description,
+      dto.projectId,
+      workspaceId,
+      req.user.userId
+    )
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(":projectId")
+  getTasks(@Param("projectId") projectId:string){
+
+    return this.tasksService.getTasks(projectId)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(":taskId/status")
+  updateStatus(
+    @Param("taskId") taskId:string,
+    @Body("status") status:string
+  ){
+
+    return this.tasksService.updateStatus(taskId,status)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(":taskId")
+  deleteTask(@Param("taskId") taskId:string){
+
+    return this.tasksService.deleteTask(taskId)
+  }
+
+}
