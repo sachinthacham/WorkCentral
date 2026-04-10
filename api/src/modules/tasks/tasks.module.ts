b44f4c0e18ad5c9b9b48dcc1c4495225c7c8d47a
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-10 23:01:33 +0530

    add notification service

--- api/src/modules/tasks/tasks.module.ts
+++ api/src/modules/tasks/tasks.module.ts
@@ -1,17 +1,30 @@
-import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose'
-import { Task, TaskSchema } from './schemas/task.schema'
-import { TasksService } from './tasks.services'
-import { TasksController } from './tasks.controller'
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from './schemas/task.schema';
+import { TasksService } from './tasks.services';
+import { TasksController } from './tasks.controller';
+import { TaskComment, TaskCommentSchema } from './schemas/task-comment.schema';
+import {
+  TaskActivity,
+  TaskActivitySchema,
+} from './schemas/task-activity.schema';
+import { TaskGateway } from './task.gateway';
+import { NotificationSchema } from '../notifications/schemas/notification.schema';
+import { NotificationsService } from '../notifications/notifications.service';
+import { NotificationsController } from '../notifications/notifications.controller';
+import { Notification } from '../notifications/schemas/notification.schema';
 
 @Module({
-  imports:[
+  imports: [
     MongooseModule.forFeature([
-      { name: Task.name, schema: TaskSchema }
-    ])
+      { name: Task.name, schema: TaskSchema },
+      { name: TaskComment.name, schema: TaskCommentSchema },
+      { name: TaskActivity.name, schema: TaskActivitySchema },
+      { name: Notification.name, schema: NotificationSchema },
+    ]),
   ],
 
-  providers:[TasksService],
-  controllers:[TasksController]
+  providers: [TasksService, TaskGateway, NotificationsService],
+  controllers: [TasksController, NotificationsController],
 })
 export class TasksModule {}
